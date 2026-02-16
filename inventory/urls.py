d86297bb5d9f3fe63471d01c022e8529817949f5
725y <251416069+725yoshimoto-cmd@users.noreply.github.com>
2026-02-16 09:08:28 +0900

    Create InventoryItem model, admin registration, and list view

--- inventory/urls.py
+++ inventory/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("inventory/", views.InventoryListView.as_view(), name="inventory_list"),
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView
+from .models import InventoryItem
 
 def index(request):
     return HttpResponse("StockNavi Top Page")
+
+class InventoryListView(LoginRequiredMixin, ListView):
+    model = InventoryItem
+    template_name = "inventory/list.html"
