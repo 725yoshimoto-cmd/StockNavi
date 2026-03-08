af60c49dea91b281c7cc85f8248c09f2dc8e55ac
725y <251416069+725yoshimoto-cmd@users.noreply.github.com>
2026-03-08 14:49:05 +0900

    導線修正: portfolioトップ表示とログイン画面にアカウント登録リンクを追加

--- stocknavi/urls.py
+++ stocknavi/urls.py
@@ -7,7 +7,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path("", RedirectView.as_view(url="/accounts/login/", permanent=False)),
+    path("", inv_views.PortfolioView.as_view(), name="portfolio"),
     path("admin/", admin.site.urls),
     path("inventory/", include("inventory.urls")),
 
