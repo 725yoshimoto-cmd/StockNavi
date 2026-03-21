157cc8a1aa4b480e6fe9dbe940bd8d83413ca1af
725y <251416069+725yoshimoto-cmd@users.noreply.github.com>
2026-03-21 12:55:21 +0900

    画像ファイル名を整理

--- inventory/forms.py
+++ inventory/forms.py
@@ -1,22 +1,28 @@
 from django import forms
 from .models import InventoryItem
+import datetime
 
 
 class InventoryItemForm(forms.ModelForm):
-    """
-    在庫登録/編集フォーム
-    - purchase_date / expiry_date はスマホで入力しやすいように date picker にする
-    """
+    purchase_month = forms.CharField(
+        required=False,
+        widget=forms.DateInput(attrs={"type": "month"})
+    )
+    expiry_month = forms.CharField(
+        required=False,
+        widget=forms.DateInput(attrs={"type": "month"})
+    )
+
     class Meta:
         model = InventoryItem
         fields = [
-            "category",
-            "storage_location",
             "name",
-            "quantity",
+            "category",
             "content_amount",
-            "purchase_date",
-            "expiry_date",
+            "quantity",
+            "purchase_month",
+            "expiry_month",
+            "storage_location",
             "image",
         ]
         labels = {
@@ -25,11 +31,47 @@ class InventoryItemForm(forms.ModelForm):
             "name": "在庫名",
             "quantity": "数量",
             "content_amount": "内容量",
-            "purchase_date": "購入日",
-            "expiry_date": "賞味期限",
+            "purchase_month": "購入日",
+            "expiry_month": "賞味期限",
             "image": "商品画像",
         }
-        widgets = {
-            "purchase_date": forms.DateInput(attrs={"type": "date"}),
-            "expiry_date": forms.DateInput(attrs={"type": "date"}),
-        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        if self.instance and self.instance.purchase_date:
+            self.fields["purchase_month"].initial = self.instance.purchase_date.strftime("%Y-%m")
+
+        if self.instance and self.instance.expiry_date:
+            self.fields["expiry_month"].initial = self.instance.expiry_date.strftime("%Y-%m")
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+        purchase_month = cleaned_data.get("purchase_month")
+        expiry_month = cleaned_data.get("expiry_month")
+
+        if purchase_month:
+            year, month = map(int, purchase_month.split("-"))
+            cleaned_data["purchase_date"] = datetime.date(year, month, 1)
+        else:
+            cleaned_data["purchase_date"] = None
+
+        if expiry_month:
+            year, month = map(int, expiry_month.split("-"))
+            cleaned_data["expiry_date"] = datetime.date(year, month, 1)
+        else:
+            cleaned_data["expiry_date"] = None
+
+        return cleaned_data
+
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+        instance.purchase_date = self.cleaned_data.get("purchase_date")
+        instance.expiry_date = self.cleaned_data.get("expiry_date")
+
+        if commit:
+            instance.save()
+            self.save_m2m()
+
+        return instance
